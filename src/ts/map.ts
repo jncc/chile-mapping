913f9c0954cea886a42b82d47815077fc2f5046a
Cathy@JNCC <Cathy.Jin@jncc.gov.uk>
2019-06-10 17:19:34 +0100

    Now creating the layers from content.json

--- src/ts/map.ts
+++ src/ts/map.ts
@@ -11,109 +11,43 @@ export function createMap(container: HTMLElement, config: Config) {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map)
 
-  //base layers
+  // add base maps
+  let baseMaps = {}
+  content.base_layers.forEach(baseLayer => {
+    let layer = L.tileLayer.wms('https://ows.jncc.gov.uk/chile_mapper/wms?', {
+      layers: baseLayer.layers,
+      transparent: true,
+      format: 'image/png',
+      opacity: 0.5
+    })
 
-  let dem = L.tileLayer.wms('https://ows.jncc.gov.uk/chile_mapper/wms?', {
-    layers: 'chile_mapper:dem_aoi_10m_tiled',
-    transparent: true,
-    format: 'image/png',
-    opacity: 0.5
+    // start with this as default
+    if (baseLayer.id == 'dem') {
+      layer.addTo(map)
+    }
+
+    Object.assign(baseMaps, {[baseLayer[config.language]]: layer})
+  })
+
+  // add overlays
+  let overlayMaps = {}
+  content.overlay_layers.forEach(overlay => {
+    let layer = L.tileLayer.wms('https://ows.jncc.gov.uk/chile_mapper/wms?', {
+      layers: overlay.layers,
+      transparent: true,
+      format: 'image/png',
+      opacity: 0.5
+    })
+
+    // start with this as default
+    if (overlay.id == 'hillshade') {
+      layer.addTo(map)
+    }
+
+    Object.assign(overlayMaps, {[overlay[config.language]]: layer})
+  })
+  
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
   }).addTo(map)
-
-  let waterYield = L.tileLayer.wms('https://ows.jncc.gov.uk/chile_mapper/wms?', {
-    layers: 'chile_mapper:baseline_hruresults',
-    transparent: true,
-    format: 'image/png',
-    opacity: 0.5
-  })
-
-  let rcp45waterYield = L.tileLayer.wms('https://ows.jncc.gov.uk/chile_mapper/wms?', {
-    layers: 'chile_mapper:rcp_45_hruresults',
-    transparent: true,
-    format: 'image/png',
-    opacity: 0.5
-  })
-
-  let rcp85waterYield = L.tileLayer.wms('https://ows.jncc.gov.uk/chile_mapper/wms?', {
-    layers: 'chile_mapper:rcp_85_hruresults',
-    transparent: true,
-    format: 'image/png',
-    opacity: 0.5
-  })
-
-  let burnAvoidance = L.tileLayer.wms('https://ows.jncc.gov.uk/chile_mapper/wms?', {
-    layers: 'chile_mapper:burn_avoidance',
-    transparent: true,
-    format: 'image/png',
-    opacity: 0.5
-  })
-
-  let ignitionSusceptibility = L.tileLayer.wms('https://ows.jncc.gov.uk/chile_mapper/wms?', {
-    layers: 'chile_mapper:ignition_susceptibility',
-    transparent: true,
-    format: 'image/png',
-    opacity: 0.5
-  })
-
-  let habitatMap = L.tileLayer.wms('https://ows.jncc.gov.uk/chile_mapper/wms?', {
-    layers: 'chile_mapper:habmap_landuse_wvineyards',
-    transparent: true,
-    format: 'image/png',
-    opacity: 0.5
-  })
-
-  let soilLoss = L.tileLayer.wms('https://ows.jncc.gov.uk/chile_mapper/wms?', {
-    layers: 'chile_mapper:usle_v2_mean_easymap',
-    transparent: true,
-    format: 'image/png',
-    opacity: 0.5
-  })
-
-  let baseMaps = {
-    [content.layers.dem[config.language]]: dem,
-    [content.layers.water_yield[config.language]]: waterYield,
-    [content.layers.rcp_45_water_yield[config.language]]: rcp45waterYield,
-    [content.layers.rcp_85_water_yield[config.language]]: rcp85waterYield,
-    [content.layers.burn_avoidance[config.language]]: burnAvoidance,
-    [content.layers.habitat_map[config.language]]: habitatMap,
-    [content.layers.soil_loss[config.language]]: soilLoss
-  }
-
-  // overlays
-  let hillshade = L.tileLayer.wms('https://ows.jncc.gov.uk/chile_mapper/wms?', {
-    layers: 'chile_mapper:dem_aoi_10m_hillshade',
-    transparent: true,
-    format: 'image/png',
-    opacity: 0.5
-  })
-
-  let roads = L.tileLayer.wms('https://ows.jncc.gov.uk/chile_mapper/wms?', {
-    layers: 'chile_mapper:roads',
-    transparent: true,
-    format: 'image/png',
-    opacity: 0.5
-  })
-
-  let subbasins = L.tileLayer.wms('https://ows.jncc.gov.uk/chile_mapper/wms?', {
-    layers: 'chile_mapper:subbasins',
-    transparent: true,
-    format: 'image/png',
-    opacity: 0.5
-  })
-
-  let rivers = L.tileLayer.wms('https://ows.jncc.gov.uk/chile_mapper/wms?', {
-    layers: 'chile_mapper:baseline_rchresults',
-    transparent: true,
-    format: 'image/png',
-    opacity: 0.5
-  })
-    
-  let overlayMaps = {
-    [content.layers.hillshade[config.language]]: hillshade,
-    [content.layers.roads[config.language]]: roads,
-    [content.layers.subbasins[config.language]]: subbasins,
-    [content.layers.rivers[config.language]]: rivers,
-  }
-
-  L.control.layers(baseMaps, overlayMaps).addTo(map)
 }
